2de2b999f03f05701df8916dee11ca1b69c409c2
Ba-So <Bastian.Sommerfeld@gmail.com>
2018-06-02 16:31:39 +0200

    adding Lorenz and irreversible

--- lorenz.py
+++ lorenz.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from mpl_toolkits.mplot3d import Axes3D
 
-class L96:
+class L96(object):
     def __init__(self, N, R, t = None):
         self.N = N
         self.R = R
@@ -20,21 +20,6 @@ class L96:
         self.t = t
         self.y = np.zeros((N, len(self.t), N))
 
-    def Lorenz(self, y, t):
-        dy = np.zeros(self.N)
-        dy[0] = ((y[1] - y[self.N-2])
-                  * y[self.N-1] - y[0])
-        dy[1] = ((y[2] - y[self.N-1])
-                  * y[0] - y[1])
-        dy[self.N-1] = ((y[0]-y[self.N-3])
-                    * y[self.N-2] - y[self.N-1])
-        for j in range(2, (self.N-1)):
-            dy[j] = (
-                (y[j+1] - y[j-2])
-                * y[j-1] - y[j]
-            )
-        dy = dy + self.F[self.j]
-        return dy
 
     def Integrate(self):
         self.j = 0
@@ -44,6 +29,11 @@ class L96:
                 self.Lorenz, ci, self.t,
                 rtol = 1e-8, atol = 1e-8)
             self.j = self.j + 1
+
+    def Lorenz(self, y, t):
+        #tbd in child classes
+
+        return y
 
     def Momentum(self):
         self.Mom = np.einsum('ijk -> ik',self.y) / self.N
@@ -62,7 +52,109 @@ class L96:
         ax.set_zlabel('$x_3$')
         plt.show()
 
+class L96_irr(L96):
 
+    def __init__(self, N, R, t = None):
+        super(L96_irr, self).__init__(N, R, t)
+        self.J = np.array([
+            [ for k in range(N)]
+            for j in range(N)
+        ])
+
+    def Lorenz(self, y, t):
+        dy = np.zeros(self.N*(self.N+1))
+        dy[0] = ((y[1] - y[self.N-2])
+                  * y[self.N-1] - y[0])
+        dy[1] = ((y[2] - y[self.N-1])
+                  * y[0] - y[1])
+        dy[self.N-1] = ((y[0]-y[self.N-3])
+                    * y[self.N-2] - y[self.N-1])
+        for j in range(2, (self.N-1)):
+            dy[j] = (
+                (y[j+1] - y[j-2])
+                * y[j-1] - y[j]
+            )
+        dy = dy + self.F[self.j]
+
+        # the Jacobian is riding piggy back...
+        dy[self.N:self.N*self.N] = self.Jacobian(y)
+
+        return dy
+
+    def Jacobian(self, y):
+
+        Y = np.empty([self.N, self.N])
+        Y.fill(0.0)
+        for j in range(self.N):
+            for k in range(self.N):
+                Y[j,k] = y[self.N-1 + j + (k-1)*(self.N-1)]
+
+        Jac1 = np.empty([self.N, self.N])
+        Jac1.fill(0.0)
+        Jac1 = np.fill_diagonal(Jac1, -1)
+        Jac2 = np.empty([self.N, self.N])
+        Jac2.fill(0.0)
+        Jac2[0, 1] = y[self.N-1]
+        Jac2[0, self.N - 1] = y[1]
+        Jac2[0, self.N - 2] = -y[self.N-1]
+
+        Jac2[1, 2] = y[0]
+        Jac2[1, self.N - 1] = -y[0]
+        Jac2[1, 0] = y[2] - y[self.N-1]
+
+        Jac2[self.N-1, 0] = y[self.N-2]
+        Jac2[self.N-1, self.N - 3] = -y[self.N-2]
+        Jac2[self.N-1, self.N - 2] = y[0] - y[self.N-3]
+
+        for j in range(2,self.N-1):
+            Jac2[j, j+1] = y[j-1]
+            Jac2[j, j-1] = y[j+1]-y[j-2]]
+            Jac2[j, j-2] = -y[j-1]
+
+        Jac = Jac1 + Jac2
+
+        # expected shape of that product is 1D len(self.N*(self.N-1))
+        return Jac * Y
+
+
+class L96_rev(L96):
+
+    def __init__(self, N, R, t = None):
+        super(L96_rev, self).__init__(N, R, t)
+
+
+    def Lorenz(self, y, t):
+        Fg2 = (
+            np.ones(self.N)
+            * np.divide(
+                np.einsum('j -> ',y),
+                np.einsum('j, j -> ', y,y)
+              )
+       )
+        dy = np.zeros(self.N)
+        dy[0] = (
+            (y[1] - y[self.N-2]) * y[self.N-1]
+            - Fg2[0]*y[0]
+            + self.F[0]
+        )
+        dy[1] = (
+            (y[2] - y[self.N-1]) * y[0]
+            - Fg2[1] * y[1]
+            + self.F[1]
+        )
+        dy[self.N-1] = (
+            (y[0]-y[self.N-3]) * y[self.N-2]
+            - Fg2[self.N - 1] * y[self.N-1]
+            + self.F[self.N -1]
+        )
+        for j in range(2, (self.N-1)):
+            dy[j] = (
+                (y[j+1] - y[j-2]) * y[j-1]
+                - Fg2[j]*y[j]
+                + self.F[j]
+            )
+        dy = dy + self.F[self.j]
+        return dy
 
 
 
--- run_lorenz.py
+++ run_lorenz.py
@@ -4,6 +4,18 @@ import lorenz as lz
 
 if __name__ == '__main__':
 
-    lz_irr = lz.L96(10,8)
+    lz_irr = lz.L96_irr(32,8)
     lz_irr.Integrate()
     lz_irr.Plot()
+    lz_irr.Momentum()
+    lz_irr.Energy()
+    print(lz_irr.meanEgy)
+    print(lz_irr.meanMom)
+
+    lz_rev = lz.L96_rev(32,8)
+    lz_rev.Integrate()
+    lz_rev.Plot()
+    lz_rev.Momentum()
+    lz_rev.Energy()
+    print(lz_rev.meanEgy)
+    print(lz_rev.meanMom)
